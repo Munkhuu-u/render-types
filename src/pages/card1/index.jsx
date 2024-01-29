d394859ea2899b864fa689243f60b02f2fdbd4eb
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-01-29 18:32:52 +0800

    before restart com

--- src/pages/card1/index.jsx
+++ src/pages/card1/index.jsx
@@ -1,16 +1,25 @@
-export default function Todo(props) {
-  console.log(props);
-  return (
-    <div>
-      <h1>card 1</h1>
-    </div>
-  );
-}
+import { useRouter } from "next/router";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://dev.to/api/articles?username=ben");
   const data = await res.json();
-  console.log("response", res);
-  console.log(data);
   return { props: { data } };
 };
+
+export default function Todo(props) {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>card 1</h1>
+      <p>{`Title: ${props.data[0].title}`}</p>
+      <p>Title: {props.data[0].title}</p>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        goto HOME
+      </button>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
-// import React from "react";
-// import { useRouter } from "next/router";
+import React from "react";
+import { useRouter } from "next/router";
 
 export default function Home(props) {
-  // const router = useRouter();
+  const router = useRouter();
   return (
-    <div>
-      {/* className="h-[400px] flex items-center justify-center bg-gray-50 text-gray-900" */}
-      <p>HOME Page</p>
-      {/* <button onClick={router.push("/page1")}>go to card 1</button> */}
+    <div
+      className="h-[400px] flex flex-col items-center justify-center bg-gray-50
+    text-gray-900"
+    >
+      <p>HOME PAGE </p>
+      <button
+        onClick={() => {
+          router.push("/card1");
+        }}
+      >
+        goto card 1
+      </button>
     </div>
   );
 }
