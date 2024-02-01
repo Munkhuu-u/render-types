4affbb4d4431d1bd911b034d6cb8ca4aeaa57351
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-02-01 08:41:18 +0800

    made seperate pages,pending make dynamic page & use layout, carousel

--- src/pages/allblog/fashion.jsx
+++ src/pages/allblog/fashion.jsx
@@ -1,20 +1,17 @@
 import { Blog } from "@/components";
-
 export const getServerSideProps = async () => {
   const res = await fetch(
-    "https://dev.to/api/articles?per_page=15&top=&tag=travel"
+    "https://dev.to/api/articles?per_page=15&top=15&tag=fashion"
   );
   const data = await res.json();
-  console.log("logging from travel getserversideprops: ", data);
   return { props: { data } };
 };
 
 export default function page({ data }) {
-  console.log("log from travel fn", data);
   return (
     <div className="flex flex-wrap gap-5">
       {data.map((aData) => {
-        return <Blog aData={aData} tag="Travel" />;
+        return <Blog aData={aData} tag="Fashion" />;
       })}
     </div>
   );
