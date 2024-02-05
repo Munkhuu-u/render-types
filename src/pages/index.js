b2f66307b29b6b0b74686e893bd33072b1a168ba
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-02-05 15:42:24 +0800

    classwork

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,41 +6,40 @@ import { LoadMore } from "./LoadMore";
 import React, { useState, useEffect } from "react";
 // import { Footer } from "./Footer.js";
 
-// export const getStaticProps = async () => {
-//   const His = await fetch("https://dev.to/api/articles?per_page=1&top=1");
-//   const highlight = await His.json();
-//   const Trs = await fetch("https://dev.to/api/articles?per_page=4&top=1");
-//   const trends = await Trs.json();
-//   const Bls = await fetch("https://dev.to/api/articles?per_page=15");
-//   const blogs = await Bls.json();
-//   return { props: { highlight, trends, blogs } };
-// };
+export const getStaticProps = async () => {
+  // const His = await fetch("http://dev.to/api/articles?per_page=1&top=1");
+  const His = await fetch("http://localhost:4000/api/highlight");
+  const highlight = await His.json();
+  const Trs = await fetch("http://localhost:4000/api/trend");
+  const trends = await Trs.json();
+  const Bls = await fetch("http://localhost:4000/api/blog");
+  const blogs = await Bls.json();
+  return { props: { highlight, trends, blogs } };
+};
 
 export default function Home({ highlight, trends, blogs }) {
-  const [article, setArticle] = useState();
+  // const [highlight, setHighlight] = useState();
+  // const [article, setArticle] = useState();
+  // const [article, setArticle] = useState();
 
-  useEffect(() => {
-    async function getArticle() {
-      const res = await fetch("http://localhost:4000/api/blog");
-      const article = await res.json();
-      console.log("article: ", article);
-      // console.log("article.title", article.title);
-      setArticle(article);
-    }
-    getArticle();
-  }, []);
+  // useEffect(() => {
+  //   async function getArticle() {
+  //     const res = await fetch("http://localhost:4000/api/main");
+  //     const data = await data.json();
+  //     setHighlight(article);
+  //   }
+  //   getArticle();
+  // }, []);
 
-  console.log("article", article);
-  // getArticle();
+  // console.log("article: ", article);
 
   return (
     <div className="flex flex-col gap-[100px]">
-      {/* <Highlight data={highlight} />
+      <Highlight data={highlight} />
       <Trends data={trends} />
       <Blogs data={blogs} />
-    <LoadMore /> */}
-      <p>FRONT END</p>
-      <p>{article.title}</p>
+      {/* <LoadMore /> */}
+      {/* <p>{article[0].title}</p> */}
     </div>
   );
 }
--- src/utils/todo.js
+++ src/utils/todo.js
@@ -8,3 +8,18 @@
 // Header Footer-iig lay-out-r hiih
 // Blog-uudiin filtertei page-uudiig dynamic layout-r hiih
 // Detail page-iig dynamic layout-r hiih
+
+// in main index
+//  http://localhost:4000/api/blog
+//  highlight
+//  https://dev.to/api/articles?per_page=1&top=1
+//  trend
+//  https://dev.to/api/articles?per_page=4&top=1
+//  blog
+//  https://dev.to/api/articles?per_page=15
+
+// in blog page
+//  blog index
+//  fashion
+//  design
+//  travel
